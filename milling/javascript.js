a0c8bf022a3aee22542a48f901b031f2f4f2aab6
Nikhil Kumar <nknapster@gmail.com>
2021-09-21 00:40:35 +0530

    miling and reflection

---
+++ milling/javascript.js
@@ -0,0 +1,11 @@
+const b = new Date;
+
+console.log(b);
+console.log("hello");
+
+function displayTime(){
+    c = document.getElementsByClassName("displaytime")[0].innerHTML
+    d = new Date
+    { document.getElementsByClassName("displaytime")[0].innerHTML= c+"<small>" + d +"</small><br>" ;}
+
+}
